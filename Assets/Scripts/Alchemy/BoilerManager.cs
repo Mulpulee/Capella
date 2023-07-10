1b52a7a3b30f8085b0b310d6425bbecf847fb12d
Mulpulee <0303vf@gmail.com>
2023-07-11 02:05:05 +0900

    GetTable & Drip

--- Assets/Scripts/Alchemy/BoilerManager.cs
+++ Assets/Scripts/Alchemy/BoilerManager.cs
@@ -1,3 +1,4 @@
+using Automation.DataTable;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,13 +7,14 @@ using UnityEngine.WSA;
 
 public class BoilerManager : MonoBehaviour
 {
-    [SerializeField] private GameObject water;
-    [SerializeField] private GameObject oil;
+    [SerializeField] private SpriteRenderer water;
+    [SerializeField] private SpriteRenderer oil;
     [SerializeField] private GameObject product;
 
     private MouseManager mouse;
     private GameObject p;
     private int stackCount = 0;
+    private int powderCode = 0;
 
     private void Start()
     {
@@ -28,19 +30,47 @@ public class BoilerManager : MonoBehaviour
         Transform powder = transform.GetChild(stackCount++);
         powder.GetComponent<SpriteRenderer>().color = input._powderColor;
         powder.GetComponent<Ingredient>().SetData(input);
+
+        if (powderCode > input._id) powderCode += input._id * 10;
+        else
+        {
+            powderCode *= 10;
+            powderCode += input._id;
+        }
     }
 
     private void Drip()
     {
-        if (stackCount == 0)
-        {
-            p = Instantiate(product);
-            p.GetComponent<Product>();
+        if (stackCount == 0) oil.enabled = true;
+        else water.enabled = true;
 
-            return;
-        }
+        mouse.RemoveHolding();
 
+        MenuRow menu;
+        if (stackCount == 0) menu = DataSystem<MenuRow>.GetRow(10088);
+        else menu = DataSystem<MenuRow>.GetRow(10000 + powderCode);
 
+        p = Instantiate(product);
+        Product pp = p.GetComponent<Product>();
+        pp._name = menu.Name;
+        pp._description = menu.Description;
+        pp._ingreds = new int[2];
+        pp._ingreds[0] = menu.Ingredient1;
+        pp._ingreds[1] = menu.Ingredient2;
+        pp._gasColor = new Color(menu.GasColor_r, menu.GasColor_g, menu.GasColor_b);
+        pp._isGas = false;
+
+        ResetBoiler();
+
+        return;
+    }
+
+    private void ResetBoiler()
+    {
+        stackCount = 0;
+        powderCode = 0;
+        transform.GetChild(0).GetComponent<SpriteRenderer>().color = Color.clear;
+        transform.GetChild(1).GetComponent<SpriteRenderer>().color = Color.clear;
     }
 
     private void OnMouseDown()
